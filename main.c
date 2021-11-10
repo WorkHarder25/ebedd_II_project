e97fea2040ca71e3fc9c3a6ffff802853ff73a2b
WorkHarder25 <lsarver14@gmail.com>
2021-11-10 11:04:34 -0600

    Update main.c

--- main.c
+++ main.c
@@ -43,131 +43,17 @@ void initHw()
 {
      //Initialize system clock to 40 MHz
     initSystemClockTo40Mhz();//change to 400 kHz
-}
 
-void getsUart0(char str[], uint8_t size)
-{
-    uint8_t count = 0;
-    bool end = false;
-    char c;
-    while(!end)
-    {
-        c = getcUart0();
-        end = (c == 13) || (count == size);
-        if (!end)
-        {
-            if ((c == 8 || c == 127) && count > 0)
-                count--;
-            if (c >= ' ' && c < 127)
-                str[count++] = c;
-        }
-    }
-    str[count] = '\0';
-}
-
-uint8_t asciiToUint8(const char str[])
-{
-    uint8_t data;
-    if (str[0] == '0' && tolower(str[1]) == 'x')
-        sscanf(str, "%hhx", &data);
-    else
-        sscanf(str, "%hhu", &data);
-    return data;
-}
-
-//I2C
-void initI2c0(void)
-{
-    // Enable clocks
-    SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R0;
-    _delay_cycles(3);
-    enablePort(PORTB);
-
-    // Configure I2C
-    selectPinPushPullOutput(I2C0SCL);
-    setPinAuxFunction(I2C0SCL, GPIO_PCTL_PB2_I2C0SCL);
-    selectPinOpenDrainOutput(I2C0SDA);
-    setPinAuxFunction(I2C0SDA, GPIO_PCTL_PB3_I2C0SDA);
-
-    // Configure I2C0 peripheral
-    I2C0_MCR_R = 0;                                     // disable to program
-    I2C0_MTPR_R = 199;                                  // (40MHz/2) / (199+1) = 100kbps //change to 400khz
-    I2C0_MCR_R = I2C_MCR_MFE;                           // master
-    I2C0_MCS_R = I2C_MCS_STOP;
-}
-
-bool pollI2c0Address(uint8_t add)
-{
-    I2C0_MSA_R = (add << 1) | 1; // add:r/~w=1
-    I2C0_MICR_R = I2C_MICR_IC;
-    I2C0_MCS_R = I2C_MCS_START | I2C_MCS_RUN | I2C_MCS_STOP;
-    while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    return !(I2C0_MCS_R & I2C_MCS_ERROR);
-}
-
-bool isI2c0Error(void)
-{
-    return (I2C0_MCS_R & I2C_MCS_ERROR);
-}
-
-
-void writeI2c0Registers(uint8_t add, uint8_t reg, uint8_t data[], uint8_t size)//changes for eepromp
-{
-    uint8_t i;
-    I2C0_MSA_R = add << 1; // add:r/~w=0
-    I2C0_MDR_R = reg;
-    if (size == 0)
-    {
-        I2C0_MICR_R = I2C_MICR_IC;
-        I2C0_MCS_R = I2C_MCS_START | I2C_MCS_RUN | I2C_MCS_STOP;
-        while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    }
-    else
-    {
-        I2C0_MICR_R = I2C_MICR_IC;
-        I2C0_MCS_R = I2C_MCS_START | I2C_MCS_RUN;
-        while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-        for (i = 0; i < size-1; i++)
-        {
-            I2C0_MDR_R = data[i];
-            I2C0_MICR_R = I2C_MICR_IC;
-            I2C0_MCS_R = I2C_MCS_RUN;
-            while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-        }
-        I2C0_MDR_R = data[size-1];
-        I2C0_MICR_R = I2C_MICR_IC;
-        I2C0_MCS_R = I2C_MCS_RUN | I2C_MCS_STOP;
-        while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    }
-}
-
-uint8_t readI2c0Register16(uint8_t add, uint16_t reg)
-{
-    I2C0_MSA_R = add << 1; // Write address
-    I2C0_MDR_R = (reg >> 8) & 0xFF; // Get HB of add
-    I2C0_MICR_R = I2C_MICR_IC;
-    I2C0_MCS_R = I2C_MCS_START | I2C_MCS_RUN; // Transmit HB of add
-    while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    I2C0_MDR_R = reg & 0xFF; // Get LB of add
-    I2C0_MICR_R = I2C_MICR_IC;
-    I2C0_MCS_R = I2C_MCS_RUN; // Transmit LB
-    while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    I2C0_MSA_R = (add << 1) | 1; // BEGIN READ
-    I2C0_MICR_R = I2C_MICR_IC;
-    I2C0_MCS_R = I2C_MCS_START | I2C_MCS_RUN | I2C_MCS_STOP; // Complete read and stop
-    while ((I2C0_MRIS_R & I2C_MRIS_RIS) == 0);
-    return I2C0_MDR_R;
-}
-
-
-int main(void)
-{
-    initHw();//initialize hardware
     initI2c0();//initialize I2C
 
     enablePort(PORTA);// Enables port A sets LevelSelect as an output
     selectPinPushPullOutput(LEVELSELECT);
     setPinValue(LEVELSELECT, 1);
+}
+
+int main(void)
+{
+    initHw();//initialize hardware
 
     uint8_t checkpoll;//variable and function for checking polling to see if the EEPROM is connected
 
@@ -180,6 +66,6 @@ int main(void)
     writeI2c0Registers(0xA0 >> 1, HB(address), i2cData, 2);//Writes to address in EEPROM using address high byte, array of address low byte, and 2 for size
     uint16_t data = readI2c0Register16(0xA0 >> 1, address);//Reads value stored in address of EEPROM
 
-	return 0;
+    return 0;
 }
 

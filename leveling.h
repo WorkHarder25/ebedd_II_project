b4864a15210dfbe1d0970cbca3b2b39246490ba0
jesusguerramaverick <76236095+jesusguerramaverick@users.noreply.github.com>
2021-12-03 13:38:30 -0600

    Create leveling.h

---
+++ leveling.h
@@ -0,0 +1,15 @@
+/*
+ * leveling.h
+ *
+ *  Created on: Dec 3, 2021
+ *      Author: Arcan
+ */
+
+#ifndef LEVELING_H_
+#define LEVELING_H_
+
+int randomArrayElement(int arr[]);
+
+
+
+#endif /* LEVELING_H_ */
